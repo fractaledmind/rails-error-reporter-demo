d10454ee14b49297b1562bda01e851c238e7f668
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-09 10:54:32 +0100

    Add a basic route that throws an error if query param set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def root
+    raise StandardError, "This is a test exception" if params[:error]
+
+    render plain: "Hello, World!"
+  end
 end
